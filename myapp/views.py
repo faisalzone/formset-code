5a16d1cf996c81040a71496d484dfb3ee5071d46
faisalzone <faisalzone@gmail.com>
2021-09-26 23:12:48 +1000

    rendering each form field

--- myapp/views.py
+++ myapp/views.py
@@ -13,7 +13,7 @@ def index(request):
     #            fields=('name', 'location', 'photo'),
     #                                       extra=1)
 
-    ExampleFormSet = modelformset_factory(Example, form=ExampleForm, extra=1)
+    ExampleFormSet = modelformset_factory(Example, form=ExampleForm, extra=0)
 
     # queryset here will make sure no saved object is displayed
     # For a creation form, queryset is better to be none as we want the users
@@ -22,15 +22,17 @@ def index(request):
 
     if request.method == 'POST':
         form = ExampleFormSet(request.POST, request.FILES)
-        # form.save() will save all of the values. similar to modelform
-        # To save individually, we can do a instances = form.save(commit=false)
-        # instances = form.save(commit=False)
-        #     for instance in instances:
-        #         instance.save()
+        if form.is_valid():
+            # form.save() will save all of the values. similar to modelform
+            # To save individually, we can do a instances = form.save(commit=false)
+            # instances = form.save(commit=False)
+            #     for instance in instances:
+            #         instance.save()
 
-        # instances = form.save()
-        form.save()
-    form = ExampleFormSet(queryset=Example.objects.none())
+            # instances = form.save()
+            form.save()
+            return redirect("https://google.com/")
+    form = ExampleFormSet(queryset=Example.objects.filter(name="First"))
     context = {
         'form': form,
     }
